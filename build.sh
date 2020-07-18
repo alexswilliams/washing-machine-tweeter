0dd39f3e58e018a685d1dcb7272e62570df330ed
Alex Williams <alex.williams@skybettingandgaming.com>
2020-07-18 04:23:09 +0100

    Switch to buildkit

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# If this script fails to run on the `docker buildx` line, try:
-# - enabling Experimental Features within docker
-# - running `docker buildx create` followed by `docker buildx use ...` to create a builder instance
-
 set -ex
 
 function buildAndPush {
@@ -13,20 +9,21 @@ function buildAndPush {
     local latest="last-build"
     if [ "$3" == "latest" ]; then latest="latest"; fi
 
-    docker buildx build \
-        --platform=linux/amd64 \
+    DOCKER_BUILDKIT=1 docker build \
         --build-arg NODE_VERSION=${nodeVersion} \
         --build-arg VERSION=${version} \
         --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
         --build-arg VCS_REF=$(git rev-parse --short HEAD) \
         --tag ${imagename}:${version}-${nodeVersion} \
         --tag ${imagename}:${latest} \
-        --push \
         --file Dockerfile .
+
+    DOCKER_BUILDKIT=1 docker push ${imagename}:${version}-${nodeVersion}
+    DOCKER_BUILDKIT=1 docker push ${imagename}:${latest}
 }
 
 # buildAndPush "0.1.0" "13.7.0-alpine"
 # buildAndPush "0.1.0" "14.3.0-alpine"
 buildAndPush "0.1.1" "14.5.0-alpine" "latest"
 
-# curl -X POST "https://hooks.microbadger.com/images/alexswilliams/washing-machine-tweeter/7athI2g_9MllKFtomgGSyHy56q8="
+# curl -X POST "https://hooks.microbadger.com/images/alexswilliams/washing-machine-tweeter/"
